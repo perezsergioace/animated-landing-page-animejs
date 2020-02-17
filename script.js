3c5b0bdc26e54823fa5704e5d4519f120b1a52b1
Sergio <sergio@Sergios-MBP.home>
2020-02-17 15:32:50 -0500

    cleaned up formating of code

--- script.js
+++ script.js
@@ -23,53 +23,57 @@ anime
 		delay: function(el, i) {
 			return 700 + 50 * i
 		}
-    })
-    
-TweenMax.to(".loader", 2.2, {
-    delay: 5,
-    top: "-100%",
-    ease: Expo.easeInOut
+	})
+
+TweenMax.to('.loader', 2.2, {
+	delay: 5,
+	top: '-100%',
+	ease: Expo.easeInOut
 })
 
-TweenMax.from(".logo", 2, {
-    delay: 6,
-    y: 10,
-    opacity: 0,
-    ease: Expo.easeInOut
+TweenMax.from('.logo', 2, {
+	delay: 6,
+	y: 10,
+	opacity: 0,
+	ease: Expo.easeInOut
 })
 
-TweenMax.from(".lang", 2, {
-    delay: 6.1,
-    y: 10,
-    opacity: 0,
-    ease: Expo.easeInOut
+TweenMax.from('.lang', 2, {
+	delay: 6.1,
+	y: 10,
+	opacity: 0,
+	ease: Expo.easeInOut
 })
 
-TweenMax.from(".left-img-cap", 2, {
-    delay: 6.2,
-    y: 10,
-    opacity: 0,
-    ease: Expo.easeInOut
+TweenMax.from('.left-img-cap', 2, {
+	delay: 6.2,
+	y: 10,
+	opacity: 0,
+	ease: Expo.easeInOut
 })
 
-TweenMax.staggerFrom(".social-media ul li", 2, {
-    delay: 6.3,
-    opacity: 0,
-    y: 20,
-    ease: Power3.ease
-}, 0.1)
+TweenMax.staggerFrom(
+	'.social-media ul li',
+	2,
+	{
+		delay: 6.3,
+		opacity: 0,
+		y: 20,
+		ease: Power3.ease
+	},
+	0.1
+)
 
-TweenMax.from(".left-bottom-text", 2, {
-    delay: 6.4,
-    y: 10,
-    opacity: 0,
-    ease: Expo.easeInOut
+TweenMax.from('.left-bottom-text', 2, {
+	delay: 6.4,
+	y: 10,
+	opacity: 0,
+	ease: Expo.easeInOut
 })
 
-TweenMax.from(".right-bottom-text", 2, {
-    delay: 6.7,
-    y: 10,
-    opacity: 0,
-    ease: Expo.easeInOut
-});
-
+TweenMax.from('.right-bottom-text', 2, {
+	delay: 6.7,
+	y: 10,
+	opacity: 0,
+	ease: Expo.easeInOut
+})
